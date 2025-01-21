c62114ab7e12660ffc0587e638facebdcffe761f
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-20 21:14:19 -0500

    Manejo de precios en la clase productos

--- EstructuraDatos/claseProducto.cs
+++ EstructuraDatos/claseProducto.cs
@@ -3,10 +3,13 @@ class Producto
     public int id { get; set; }
     // nombre fue creado con prop
     public string nombre { get; set; }
+    public double[] precios { get; set; }
+
 
     //constructor
-    Producto(int id, string nombre){
+    Producto(int id, string nombre, double[] precios){
         this.id = id;
         this.nombre = nombre;
+        this.precios = precios;
     }
 }
