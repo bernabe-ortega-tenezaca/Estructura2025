49a030e404a0dcc8b3b3713558083388568b81a8
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-20 20:57:54 -0500

    Definición clase persona

--- EstructuraDatos/Program.cs
+++ EstructuraDatos/Program.cs
@@ -1,7 +1,17 @@
 ﻿Console.WriteLine("Universidad Estatal Amazónica");
 
 class Persona{
+    public string Cedula { get; set; }
+    public string Nombres { get; set; }
+    public string Direccion { get; set; }
+    public string Telefono { get; set; }
 
+    Persona(string cedula, string nombres, string direccion, string telefono){
+        Cedula = cedula;
+        this.Nombres = nombres;
+        this.Direccion = direccion;
+        Telefono = telefono;
+    } 
 }
 
 class Producto
