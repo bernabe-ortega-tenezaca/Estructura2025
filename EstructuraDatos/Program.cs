1737e4354853248e3246b5eb7cae026d67b94f2d
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-20 20:52:28 -0500

    Definicion clase producto

--- EstructuraDatos/Program.cs
+++ EstructuraDatos/Program.cs
@@ -6,7 +6,15 @@ class Persona{
 
 class Producto
 {
-    
+    public int id { get; set; }
+    // nombre fue creado con prop
+    public string nombre { get; set; }
+
+    //constructor
+    Producto(int id, string nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
 
 class Perro{
