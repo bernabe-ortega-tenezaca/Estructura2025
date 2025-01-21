2b357a5b212a439c5b96150b57c7b6945e07dc3c
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-20 21:29:22 -0500

    Array de personas

--- EstructuraDatos/clasePersona.cs
+++ EstructuraDatos/clasePersona.cs
@@ -1,13 +1,17 @@
-class Persona{
+public class Persona{
     public string Cedula { get; set; }
     public string Nombres { get; set; }
     public string Direccion { get; set; }
     public string Telefono { get; set; }
 
-    Persona(string cedula, string nombres, string direccion, string telefono){
+    public Persona(string cedula, string nombres, string direccion, string telefono){
         Cedula = cedula;
         this.Nombres = nombres;
         this.Direccion = direccion;
         Telefono = telefono;
+    }
+
+    public string getPersona(){
+        return "Nombre: " + Nombres + " Dirección: " + Direccion;
     } 
 }
