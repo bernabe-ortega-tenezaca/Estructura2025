46f721b9e1a3ff63877e5eacfb7c52da3965c063
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-21 19:00:43 -0500

    Pila de personas

---
+++ EstructuraDatos/01_CEncuentro/semana 7/pilasOtroEjercicio.cs
@@ -0,0 +1,11 @@
+public static class PilasOtro{
+    public static void run(){
+        Console.WriteLine("Pilas - Más ejercicios");
+        Stack<Persona> personas = new Stack<Persona>();
+
+        Persona p1 = new Persona("03040300", "Camilo", "Puyo", "0987678");
+
+        personas.Push(p1);
+
+    }
+}
