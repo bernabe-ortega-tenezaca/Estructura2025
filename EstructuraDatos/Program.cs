3627911757c842a25e26083adda26ac1d229b625
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-25 08:17:25 -0500

    Organización en carpetas

--- EstructuraDatos/Program.cs
+++ EstructuraDatos/Program.cs
@@ -9,32 +9,4 @@
 //Pilas.run();
 //PilasOtro.run();
 
-Stack<int> pilaEnteros = new Stack<int>();
-
-pilaEnteros.Push(100);
-pilaEnteros.Push(3);
-pilaEnteros.Push(7);
-
-foreach ( var item in pilaEnteros)
-{
-    Console.WriteLine(item);
-}
-
-//Mostrar el elemento de la parte superior sin eliminar
-int ultimoelemento = pilaEnteros.Peek();
-Console.WriteLine($"El último elemento es: {ultimoelemento}");
-
-foreach ( var item in pilaEnteros)
-{
-    Console.WriteLine(item);
-}
-
-//Elimina el último elemento
-int numeroelminado = pilaEnteros.Pop();
-Console.WriteLine();
-foreach ( var item in pilaEnteros)
-{
-    Console.WriteLine(item);
-}
-
-Console.WriteLine($"\n se ha eliminado el {numeroelminado}");
+pilasClaseEncuentro.run();
